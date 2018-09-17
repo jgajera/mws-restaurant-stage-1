ec84ac0102634e012c1466422be7b536e1980123
Janki <jnkdesignhouse@gmail.com>
2018-09-16 19:56:22 -0600

    added MapBox API keys -- map is showing up!

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiamdhamVyYSIsImEiOiJjam01bXk0NGcwdnp6M3BxcDh2NjZtZ2UwIn0.Ptl2Q6RJY1P18GjRA_moMw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
